eca9bea5be7c96145ff7888b3a95090ace56c301
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2023-10-31 05:45:50 +0530

    Project Initialised. MUI Installed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey Pratinav Let's Build A Twitter Clone Today</h1>
+
+      {/* Sidebar */}
+      <Sidebar/>
+
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react"
+import TwitterIcon from "@material-ui/icons/Twitter"
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter Icon */}
+      <TwitterIcon />
+
+      {/* Sidebar Option */}
+      {/* Sidebar Option */}
+      {/* Sidebar Option */}
+      {/* Sidebar Option */}
+      {/* Sidebar Option */}
+      {/* Sidebar Option */}
+
+      {/* Button --> Tweet */}
+    </div>
+  )
+}
+
+export default Sidebar
