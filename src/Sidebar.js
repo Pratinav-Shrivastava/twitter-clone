9851c391cb0f3e283d3298d888df2a9584e1a6d9
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2023-11-06 16:48:33 +0530

    Progress on Sidebar Options and related components.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,14 @@
 import React from "react"
 import TwitterIcon from "@material-ui/icons/Twitter"
+import SidebarOption from "./SidebarOption"
+import HomeIcon from "@material-ui/icons/Home"
+import SearchIcon from "@material-ui/icons/Search"
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone"
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
   return (
@@ -8,11 +17,14 @@ function Sidebar() {
       <TwitterIcon />
 
       {/* Sidebar Option */}
-      {/* Sidebar Option */}
-      {/* Sidebar Option */}
-      {/* Sidebar Option */}
-      {/* Sidebar Option */}
-      {/* Sidebar Option */}
+      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Explore" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+      <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="Lists" Icon={ListAltIcon} />
+      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
 
       {/* Button --> Tweet */}
     </div>
