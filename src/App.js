1bec49d6ffe4acbb4f421a800d0ee21b01ad5bfc
Pratinav Shrivastava <pratinavshrivastava6@gmail.com>
2023-11-07 17:27:04 +0530

    Sidebar Component Completed. Twitter Feed Component started. Working on TweetBox.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css"
+import Feed from "./Feed"
 import Sidebar from "./Sidebar"
+import Widgets from "./Widgets"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey Pratinav Let's Build A Twitter Clone Today</h1>
+    <div className="app">
+      {/* <h1>Hey Pratinav Let's Build A Twitter Clone Today</h1> */}
 
       {/* Sidebar */}
       <Sidebar/>
 
       {/* Feed */}
+      <Feed/>
 
       {/* Widgets */}
+      <Widgets/>
+
     </div>
   )
 }
---
+++ src/TweetBox.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./TweetBox.css"
+
+function Tweetbox() {
+  return (
+    <div className="tweetBox">
+      <h2>TweetBox</h2>
+    </div>
+  )
+}
+
+export default Tweetbox
